34df1c4da90e4bb52c1e337632c252cd04181169
Martin Chaplin <martin.chaplin44@gmail.com>
2020-01-12 20:53:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,1 @@
 
-class Person
-  def initialize (persons_name)
-    @name = persons_name
-  end
-
-  def name=(persons_name)
-    @name = persons_name
-  end
-
-  def name
-    @name
-  end
-  
-end
